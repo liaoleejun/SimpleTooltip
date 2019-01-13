4583ee5aad5990ad652c070f1ae004ce9d687740
Leejun Liao <liaoleejun@163.com>
2019-01-13 15:31:48 +0800

    简化HTML, 把所有的Cites, Descs逻辑放在JS中实现

--- script/CiteNumber.js
+++ script/CiteNumber.js
@@ -62,6 +62,7 @@ $(document).ready(function () {
     }
 
     // 3.a 生成排好序的 Cite list
+    $(document.body).append("<div><h2>Descriptions:</h2><ol id='descs'></ol></div>");
     for (let i = 0; i < citesOrdered.length; i++) {
         let id = citesOrdered[i];
         let ttt = document.createElement('li');
@@ -69,6 +70,7 @@ $(document).ready(function () {
         document.getElementById("cites").appendChild(ttt);
     }
 
+    $(document.body).append("<div><h2>References:</h2><ol id='cites'></ol></div>");
     // 3.b 生成排好序的 Descs list
     for (let i = 0; i < descsOrdered.length; i++) {
         let id = descsOrdered[i];
