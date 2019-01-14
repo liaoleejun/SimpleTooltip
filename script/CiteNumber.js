5873fcd6ce092db7de0f4072babdffd30846aa26
Leejun Liao <liaoleejun@163.com>
2019-01-14 09:47:40 +0800

    化繁为简. class, data-ref两个属性简化为一个属性data-ct
    * ct 代表 cite target
    * dt 代表 desc target

--- script/CiteNumber.js
+++ script/CiteNumber.js
@@ -4,43 +4,45 @@
  * <span>
  *
  * 过程是把
- * <span class="cite" data-ref="foo">
+ * <span data-ct="foo">
  * <span>
  * 转变成
- * <span class="cite tooltip" data-ref="foo">
+ * <span class="tooltip" data-ref="foo">
  *     <a href="#aaa">[1]</a>
  * </span>
  *
  * 或者把
- * <span class="desc" data-ref="foo">
+ * <span data-dt="foo">
  * <span>
  * 转变成
- * <span class="desc tooltip" data-ref="foo">
+ * <span class="tooltip" data-ref="foo">
  * </span>
  */
 $(document).ready(function () {
     // 1.a 引用排序: 按照引用在 HTML 文档中出现顺序, 把引用的 data-ref 生成无重复值的
     //     数组 citesOrdered
-    let cites = $('.cite');
+    // let cites = $('.cite');
+    let cites = $("[data-ct]");
     let citesOrdered = []; // citesOrdered 是由 data-ref 构成的无重复数组，按照在
                            // HTML文档中出现的顺序
     for (let i = 0; i < cites.length; i++) {
-        let dataRef = cites[i].getAttribute('data-ref');
+        let dataRef = cites[i].getAttribute('data-ct');
         // array push only if not exist
         citesOrdered.indexOf(dataRef) === -1 ? citesOrdered.push(dataRef) :
                                        console.log(dataRef + " already exists");
 
         // Add the class name "tooltip", 因为SimpleTooltip.js是以类名为tooltip来选择的
         cites[i].classList.add("tooltip");
+        $(cites[i]).attr("data-ref", dataRef);
     }
 
     // 1.b 描述排序: 按照引用在 HTML 文档中出现顺序, 把引用的 data-ref 生成无重复值数
     //     组 descsOrdered
-    let descs = $('.desc');
+    let descs = $("[data-dt]");
     let descsOrdered = [];
     let descsConceptOrdered = [];
     for (let i = 0; i < descs.length; i++) {
-        let dataRef = descs[i].getAttribute('data-ref');
+        let dataRef = descs[i].getAttribute('data-dt');
         let concept = descs[i].innerHTML;
         // array push only if not exist
         descsOrdered.indexOf(dataRef) === -1 ? descsOrdered.push(dataRef) : console.log(dataRef + " already exists");
@@ -48,12 +50,13 @@ $(document).ready(function () {
 
         // Add the class name "tooltip", 因为SimpleTooltip.js是以类名为tooltip来选择的
         descs[i].classList.add("tooltip");
+        $(descs[i]).attr("data-ref", dataRef);
     }
 
     // 2. 把 HTML 文档中的引用, 按照上一步生成的数组的顺序给标上号; 同时,
     //     生成 SimpleTooltip.js 支持的HTML元素
     for (let i = 0; i < cites.length; i++) {
-        let dataRef = cites[i].getAttribute('data-ref');
+        let dataRef = cites[i].getAttribute('data-ct');
         // 给引用标上号（注意数组是从0开始下标，所以加1）
         let num = citesOrdered.indexOf(dataRef) + 1;
         cites[i].innerHTML = '<a>[' + num + ']</a>'; // cites[i].innerHTML = '[' + num + ']';
