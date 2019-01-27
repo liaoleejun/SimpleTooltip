4a9c4b69beae4fce724cf83790be7f19dae89ac8
Leejun Liao <liaoleejun@163.com>
2019-01-28 00:14:31 +0800

    集成可选逻辑: 所有链接在新标签页打开

--- script/SimpleTooltip.js
+++ script/SimpleTooltip.js
@@ -94,6 +94,26 @@ $(document).ready(function () {
 
 
 /**
+ * 设置全部链接为新窗口打开 (可选项, 本逻辑可以注释掉)
+ * 此函数，是不有问题？比如visibility: hidden;的tooltip时就会失效。不要写while(true)的js代码，会死机，cpu会转个不停
+ */
+$(document).ready(function (){
+    function externalLinks() {
+        for (let c = document.getElementsByTagName("a"), a = 0; a < c.length; a++) {
+            let b = c[a];
+
+            // all links open in new browser tag, except the link is the same hostname
+            // b.getAttribute("href") && b.hostname !== location.hostname && (b.target = "_blank");
+
+            // all links open in new browser tag, except the link is a bookmark
+            b.getAttribute("href") && b.getAttribute("href").search(/#/i) !== 0 && (b.target = "_blank");
+        }
+    }
+    externalLinks();
+});
+
+
+/**
  * Create tooltipcontent
  * @param tooltip
  */
@@ -128,6 +148,7 @@ function removeTooltipcontent() {
         element.parentNode.removeChild(element);
     }
 }
+
 
 /**
  * Place Tooltipcontent
